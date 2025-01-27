618075869454febc59d69c9d02214bc2479f6b6c
altair823 <dlsrks0734@gmail.com>
2025-01-27 19:48:57 +0900

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                     height={38}
                     priority
                 />
-                <p>version 0.0.3</p>
+                <p>김태현의 포트폴리오</p>
                 <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                     <li className="mb-2">
                         Get started by editing{" "}
