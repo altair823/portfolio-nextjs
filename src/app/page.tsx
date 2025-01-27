f0c80c9ba282ab9891d26bd6c986154c06b92ddd
altair823 <dlsrks0734@gmail.com>
2025-01-27 18:11:55 +0900

    cicd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                     height={38}
                     priority
                 />
-                <p>version 0.0.2</p>
+                <p>version 0.0.3</p>
                 <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                     <li className="mb-2">
                         Get started by editing{" "}
